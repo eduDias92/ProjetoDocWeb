0f6920ce5fd4285ae94268af661b6fe876928db8
Eduardo Dias <mr.diasjr@gmail.com>
2017-10-13 15:22:24 -0300

    Add files via upload

---
+++ envioImagem.php
@@ -0,0 +1,52 @@
+<?php
+	require_once ('conexaobd.class.php');
+
+	$con = new ConexaoBD();
+
+	$objConexao = $con->criaConexao();
+
+	if (isset($_FILES['imagem'])) {
+		# code...
+		
+
+		$img = $_FILES['imagem'];//retorna um array com as características da imagem, tamanho, fonte, arquivo temporário gerado, nome da imagem, etc.
+		$tipo = $_FILES['imagem']['type'];
+		if ($img != NULL) {//se a imagem for carregada corretamente
+			
+			$nomeFinal = date('YmdHis').'.png';//cria uma string com o nomeFinal da imagem
+
+			if(move_uploaded_file($img['tmp_name'], $nomeFinal)){//move a imagem para a pasta do sistema com o nome predefinido anteriormente (nomeFinal), se der certo ele continua o código abaixo.
+
+				$tamanhoImg = filesize($nomeFinal);//tamanho da imagem cadastrada
+
+				$mysqlImg = addslashes(fread(fopen($nomeFinal, 'r'), $tamanhoImg));
+
+				$query = "insert into cenario (codcliente, imgCenario, tipo_img) values (106045, '$mysqlImg', '$tipo')";
+
+				$retorno = $objConexao->exec($query);
+
+				unlink($nomeFinal);
+
+				echo "Retorno da inserção: $retorno <br><br>";
+			}
+
+		}
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Envio de imagens</title>
+</head>
+<body>
+<form action="envioImagem.php" method="post" enctype="multipart/form-data">
+	<label for='imagem'>Imagem:</label>
+	<input type="file" name="imagem">
+	<br>
+	<input type="submit" value="Enviar">
+
+</form>
+</body>
+</html>
