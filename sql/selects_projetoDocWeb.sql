9ff34d6b873d194023a0b8fefcd930fca542cb5f
Eduardo Dias <mr.diasjr@gmail.com>
2018-01-24 17:50:00 -0200

    Delete selects_projetoDocWeb.sql

--- sql/selects_projetoDocWeb.sql
+++
@@ -1,2 +0,0 @@
-/*Selects utilizados no Projeto:*/
-select cliente.nome, cliente.codcliente, contato.telefone1 from cliente left join contato on cliente.codcliente = contato.codcliente order by cliente.nome
